e68a80fac91ca3db27b1e148b93b8baaf9f794f4
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-04-06 20:53:32 -0500

    agregando handlbars y la opcion de eliminar links

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Login Aqui!');
 });
 
 module.exports = router;
